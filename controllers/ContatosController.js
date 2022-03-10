0607a64418a60ad377e1b090f193fc4455db9677
elvis-brandao <elvis.brandao@outlook.com>
2022-03-10 17:22:54 -0300

    Desenvolvendo funções controller api

--- controllers/ContatosController.js
+++ controllers/ContatosController.js
@@ -1,21 +1,34 @@
+const{sequelize} = require('../database/models');
+const uid = 1;
+
 const controller = {
-    index: (req, res) => {
-        return console.log('index');
+    index: async (req, res) => {
+        let sql = `SELECT id, nome FROM contatos WHERE usuarios_id = ${uid}`;
+        let contatos = await sequelize.query(sql, {type: sequelize.QueryTypes.SELECT});
+        res.status(200).json(contatos);
     },
-    show: (req, res) => {
-        return console.log('show');
+    search: async (req, res) => {
+        let sql = `SELECT id, nome FROM contatos WHERE usuarios_id=${uid} AND nome LIKE '%${req.query.q}%'`;
+        let contato = await sequelize.query(sql, {type: sequelize.QueryTypes.SELECT});
+        res.status(200).json(contato);
+    },
+    show: async (req, res) => {
+        let sql = `SELECT id, nome FROM contatos WHERE usuarios_id = ${uid} AND id = ${req.params.id}`;
+        let contato = await sequelize.query(sql, {type: sequelize.QueryTypes.SELECT});
+        if(contato.length > 0){
+            res.status(200).json(contato[0]);
+        }else{
+            res.status(404).json({msg: 'Usuário não existente'});
+        };
     },
     create: (req, res) => {
-        return console.log('create');
+        res.send('create');
     },
     destroy: (req, res) => {
-        return console.log('destroy');
+        res.send('destroy');
     },
     update: (req, res) => {
-        return console.log('update');
-    },
-    search: (req, res) => {
-        return console.log('update');
+        res.send('update');
     }
 };
 
--- routes/ContatosRouter.js
+++ routes/ContatosRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const ContatosController = require('../controllers/ContatosController');
 
 router.get('/', ContatosController.index);
-router.get('/:id', ContatosController.show);
 router.get('/search', ContatosController.search);
+router.get('/:id', ContatosController.show);
 router.post('/', ContatosController.create);
 router.delete('/:id', ContatosController.destroy);
-router.update('/:id', ContatosController.update);
+router.put('/:id', ContatosController.update);
 
 module.exports = router;
