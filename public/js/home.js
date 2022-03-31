309b757ee975888308dfbddc345251761c34b8d7
elvis-brandao <elvis.brandao@outlook.com>
2022-03-31 11:33:00 -0300

    carregando no front os contatos do banco de dados

--- public/js/home.js
+++ public/js/home.js
@@ -1,67 +1,32 @@
 const main = document.querySelector('main');
 const modal = document.getElementById("modal");
-let link = document.getElementById("linkAbrirModal");
-let edits = document.querySelectorAll("section > a");
-let search = document.getElementById('search');
+const link = document.getElementById("linkAbrirModal");
+const edits = document.querySelectorAll("section > a");
+const search = document.getElementById('search');
 
-let mostrarModal = () => {
+const mostrarModal = () => {
     modal.style.display = "flex";
     modal.style.opacity = 1;
 };
 
-let esconderModal = (evt) => {
+const esconderModal = (evt) => {
     evt.bubbles = false;
     modal.style.display = "none"
     modal.style.opacity = 0;
 };
 
-// Simulando os contatos em uma variável
-let contatos = [
-    {
-        "id":1,
-        "nome":"Kakashi Hatake",
-        "emails":["kakashi@anbu.com"],
-        "telefones":["99999-1111","98888-1234"]
-    },
-    {
-        "id":2,
-        "nome":"Sakura Haruno",
-        "emails":["sakura@konoha.com"],
-        "telefones":["99999-2222","98888-3333"]
-    },
-    {
-        "id":3,
-        "nome":"Hinata Hyuga",
-        "emails":["Hinata@hyugas.com"],
-        "telefones":["99999-3333","98888-4444"]
-    },
-    {
-        "id":4,
-        "nome":"Vovó Tsunade",
-        "emails":["tsunade@hokages.com"],
-        "telefones":["99999-4444","98888-5555"]
-    },
-    {
-        "id":5,
-        "nome":"Shikamaru Nara",
-        "emails":["shikamaru@konoha.com"],
-        "telefones":["99999-5555","98888-6666"]
-    },
-    {
-        "id":6,
-        "nome":"Ino",
-        "emails":["ino@yamanakas.com"],
-        "telefones":["99999-6666","98888-7777"]
-    },
-    {
-        "id":7,
-        "nome":"Choji Akimichi",
-        "emails":["choji@akimichis.com"],
-        "telefones":["99999-7777","98888-8888"]
-    }
-];
+const loadContatos = async () =>{
+    try {
+        let resposta = await fetch('/contatos');
+        let contatos = await resposta.json();
+        showContatos(contatos);
+    } catch (error) {
+        console.log(error);
+    };
+};
+loadContatos();
 
-let showContatos = contatos =>{
+const showContatos = contatos =>{
     main.innerHTML = '';
     contatos.forEach(c =>{
         let section = document.createElement('section');
@@ -93,7 +58,7 @@ let showContatos = contatos =>{
     });
 };
 
-let buscaContatos = trecho => {
+const buscaContatos = trecho => {
     let contatosFiltrados = contatos.filter(
         c => c.nome.toUpperCase().includes(trecho.toUpperCase())
     );
@@ -110,5 +75,3 @@ edits.forEach(
 modal.addEventListener('click',esconderModal);
 
 search.addEventListener('keyup', e => buscaContatos(e.target.value));
-
-showContatos(contatos);
